edf8878f5e9f9cf480a72965cf9abe020487cee1
Riady <riady_saz@yahoo.com>
2015-02-17 00:12:51 +0700

    ngubah sdikit sentemail

--- Lapor Taman/sentemail.php
+++ Lapor Taman/sentemail.php
@@ -1,7 +1,21 @@
 <?php
+	$id_pengaduan = $_POST["id_pengaduan"];
+	mysql_connect("localhost","root","");
+	@mysql_select_db("lapor_tamanbdg") or die( "Unable to select database");
+	$query = "SELECT * FROM t_adu WHERE id_pengaduan='".$id_pengaduan."'";
+	$result = mysql_query($query);
+	$id_taman = mysql_result($result,0,"id_mengenai");
 	
-	$toAdress = $_POST["email"];
-	$subject = $_POST["subjek"];
+	$konten = mysql_result($result,0,"konten");
+	$query = "SELECT * FROM t_taman WHERE id_taman='".$id_taman."'";
+	$result = mysql_query($query);
+	$id_instansi = mysql_result($result,0,"id_berwenang");
+	
+	$query = "SELECT * FROM t_instansi WHERE id_instansi='".$id_instansi."'";
+	$result = mysql_query($query);
+	$toAdress = mysql_result($result,0,"email");
+	
+	$subject = "Laporan tentang taman";
 	require_once '/Swift-5.1.0/lib/swift_required.php';
 	// Create the Transport
 	$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl");
@@ -24,13 +38,10 @@
 	$message->setSubject($subject);
 	$message->setFrom(array('riadysastrak@gmail.com' => 'Riady'));
 	$message->setTo($toAdress);
-	$message->setBody('
-	Hi, tes
-	konten
-');
+	$message->setBody($konten);
 	$result = $mailer->send($message);
 	echo $result;
 	    
-
+	header("Location: daftarpengaduan.php");
     
 ?>
